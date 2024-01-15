e9208cd1e8673b53cf612606be1ab45cc43a28e5
whybe-choi <choi980912@khu.ac.kr>
2024-01-15 14:07:52 +0900

    feat: Define Features fizzbuzz()

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz():
+	for i in range(1, 15+1):
+		if i % 15  == 0:
+			print('fizzbuzz')
+		elif i % 3 == 0:
+			print('fizz')
+		elif i % 5 == 0:
+			print('buzz')
+		else:
+			print(i)
+
+if __name__ == '__main__':
+	fizzbuzz()
